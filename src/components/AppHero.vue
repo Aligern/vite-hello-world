ad037522d3bfccb48f87b190d6ef4df02b1d6e9b
Aligern <sasso.luca93@gmail.com>
2024-04-09 18:54:26 +0200

    add AppTitle , AppHero components

---
+++ src/components/AppHero.vue
@@ -0,0 +1,23 @@
+<template>
+    <header>
+        <img :src="hero" alt="Futaba">
+    </header>
+</template>
+
+<script>
+    export default {
+        name:'AppHero',
+        data() {
+            return {
+                hero: '/public/img/Futaba.jpg'
+            }
+        }
+        
+    }
+</script>
+
+<style scoped>
+img {
+    width: 50%;
+}
+</style>
